41887faffeca9b5d582e1eaf7423637709d1a9bd
Rd Islam Nasim <rdislamnasim78@gmail.com>
2018-07-15 20:36:19 +0600

    Initial commit

---
+++ app/src/main/java/com/example/rdnasim/terminalbd/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.rdnasim.terminalbd;
+
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+    SwipeRefreshLayout swipe;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        swipe = findViewById(R.id.swipe);
+        swipe.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                WebAction();
+            }
+        });
+
+        WebAction();
+    }
+
+    private void WebAction() {
+        webView = findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setAppCacheEnabled(true);
+
+        //change here your url and run code in android studio..//
+
+        webView.loadUrl("https://www.wikipedia.org");
+        swipe.setRefreshing(true);
+        webView.setWebViewClient(new WebViewClient() {
+
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+
+                webView.loadUrl("file:///android_assets/error.html");
+
+            }
+
+            public void onPageFinished(WebView view, String url) {
+                // do your stuff here
+                swipe.setRefreshing(false);
+            }
+
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            finish();
+        }
+    }
+}
